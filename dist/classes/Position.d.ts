39dc267a1c892393447d9832b93a30a8730c995b
Dldvlpr <dormoy.dev@gmail.com>
2023-09-14 11:42:16 +0200

    remove dist

--- dist/classes/Position.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Position {
-    x: number;
-    y: number;
-    constructor(x: number, y: number);
-    getx(): number;
-    gety(): number;
-    setx(valueOfX: number): void;
-    sety(valueOfY: number): void;
-}
