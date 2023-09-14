f97dfbe967c8507bdf6038abdcc39266307b3418
Dldvlpr <dormoy.dev@gmail.com>
2023-09-14 12:07:34 +0200

    config jest, add first test for draw a square

--- src/test/square.test.ts
+++ src/test/square.test.ts
@@ -1,23 +1,37 @@
 import {Square} from "../classes/Square";
-import {Position} from "../classes/Position";
-import {beforeEach, describe, it} from "node:test";
+import { Position } from "../classes/Position";
 
-describe('Square draw', () => {
-    let canvas;
-    let ctx;
+describe('Square class', () => {
 
-    beforeEach(function () {
-        canvas = document.createElement('canvas');
-        canvas.width = 300;
-        canvas.height = 300;
-        ctx = canvas.getContext('2d');
-        
-        const position = new Position(0, 0);
-        const square = new Square(position, 50, "red", 4, ctx)
+    let mockContext: CanvasRenderingContext2D;
+    let position: Position;
+    let size: number;
+    let color: string;
+    let idOfSquare: number;
 
+    beforeEach(() => {
+        mockContext = {
+            fillStyle: "",
+            fillRect: jest.fn(),
+        } as unknown as CanvasRenderingContext2D;
+
+        position = new Position(0, 0);
+        size = 50;
+        color = "black";
+        idOfSquare = 1;
     });
 
-    it('should be draw a square in canvas', () => {
-
+    it('should create a Square instance correctly', () => {
+        const square = new Square(position, size, color, idOfSquare, mockContext);
+        expect(square.getPoint()).toEqual(position);
+        expect(square.getIdOfSquare()).toBe(idOfSquare);
     });
+
+    it('should draw the square with correct values', () => {
+        const square = new Square(position, size, color, idOfSquare, mockContext);
+        square.drawSquare();
+        expect(mockContext.fillStyle).toBe(color);
+        expect(mockContext.fillRect).toHaveBeenCalledWith(position.x, position.y, size, size);
+    });
+
 });
